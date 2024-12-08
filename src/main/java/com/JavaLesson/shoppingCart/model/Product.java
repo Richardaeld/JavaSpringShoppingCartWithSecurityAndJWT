01cb1b66462cd0b6f39e36606b47453c677a1a98
Richard E <richardaeld@gmail.com>
2024-12-08 12:15:14 -0500

    finished setting up first instance of model

--- src/main/java/com/JavaLesson/shoppingCart/model/Product.java
+++ src/main/java/com/JavaLesson/shoppingCart/model/Product.java
@@ -1,9 +1,6 @@
 package com.JavaLesson.shoppingCart.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,6 +22,7 @@ public class Product {
     private String description;
 
     @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
     private Category category;
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
